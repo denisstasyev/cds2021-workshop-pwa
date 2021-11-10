343310528184ac3b6e3dfd292369f4de3e09db45
Denis Stasyev <denisstasyev@gmail.com>
2021-11-11 00:31:22 +0300

    Add simple fetch

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,12 +1,18 @@
 const assetsName = "PWA_ASSETS";
-const urls = ["/", "/js/app.js", "/js/handlers.js", "/data/activities.json"];
+const urls = ["/", "/js/app.js", "/js/handlers.js", "/data/activities.json",
+    "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"];
 
+// Install the assets
 self.addEventListener('install', event => {
     caches.open(assetsName).then(cache => {
         cache.addAll(urls);
     });
 });
 
+// Serve the assets
 self.addEventListener('fetch', event => {
+    // console.log(`fetching ${event.request.url}`);
+    const response = new Response(`Service Worker responding for ${event.request.url}`);
 
+    event.respondWith(response); // HTTP response or a promise of an HTTP request
 });
