d70d367776805ee1e1248d15a10a571ffd592577
Denis Stasyev <denisstasyev@gmail.com>
2021-11-11 00:28:33 +0300

    Add cache assets

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,4 +1,11 @@
-// console.log('Hello from Service Worker!')
+const assetsName = "PWA_ASSETS";
+const urls = ["/", "/js/app.js", "/js/handlers.js", "/data/activities.json"];
+
+self.addEventListener('install', event => {
+    caches.open(assetsName).then(cache => {
+        cache.addAll(urls);
+    });
+});
 
 self.addEventListener('fetch', event => {
 
