b99c0308e81e5c121dff720151359e228e3fd72a
Denis Stasyev <denisstasyev@gmail.com>
2021-11-11 00:43:13 +0300

    Stale while Revalidate

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -9,21 +9,41 @@ self.addEventListener('install', event => {
     });
 });
 
-// Cache first
-self.addEventListener("fetch", event => {
+// Stale while Revalidate
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request) // searching in the cache
             .then(response => {
-                if (response) {
-                    // The request is in the cache
-                    return response;
-                } else {
-                    // We need to go to the network
-                    return fetch(event.request);
-                }
+                // Even if the response is in the cache, we fetch it
+                // and update the cache for future usage
+                var fetchPromise = fetch(event.request).then(
+                    networkResponse => {
+                        caches.open("activityLauncher").then(cache => {
+                            cache.put(event.request, networkResponse.clone());
+                            return networkResponse;
+                        });
+                    });
+                // We use the currently cached version if it's there
+                return response || fetchPromise;
             })
     );
 });
+
+// Cache first
+// self.addEventListener("fetch", event => {
+//     event.respondWith(
+//         caches.match(event.request) // searching in the cache
+//             .then(response => {
+//                 if (response) {
+//                     // The request is in the cache
+//                     return response;
+//                 } else {
+//                     // We need to go to the network
+//                     return fetch(event.request);
+//                 }
+//             })
+//     );
+// });
 
 
 // Synthesizing responses
